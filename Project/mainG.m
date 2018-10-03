818cc30e32352c1428b8a716b8b0063b9edd83be
Rickard Larsson <rickard.larsson.94@gmail.com>
2018-10-03 20:36:53 +0200

    All modes done

--- Project/mainG.m
+++ Project/mainG.m
@@ -7,15 +7,16 @@ set(groot,'defaultFigureVisible','on')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                              TRANSMITTER
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-img_O = double(imread('RRY025/cameraman.tif'));
+% img_O = double(imread('RRY025/cameraman.tif'));
+img_O = double(imread('RRY025/gisela_boat.tif'));
 
 
 %% DPCM predict the prediction of the reciever
+clc
 
-
-initialCondition = 'firstpixel';               % firstpixel or rowpixel
-mode = 'intuitive';                            % intuitive or smart
-
+initialCondition = 'rowpixel';               % firstpixel or rowpixel
+mode = 'DPCM';                            % intuitive or DPCM
+level = '1bit';
 
 
 [row, col] = size(img_O);
@@ -27,41 +28,49 @@ switch initialCondition
     case 'firstpixel'
         prediction = img(1);  
         errorQuantized = [prediction; zeros(st-1,1)];   % Quantized error
-        if strcmp(mode,'smart') == 1
-            
+        if strcmp(mode,'DPCM') == 1
             for n=2:st  
-            predictionError = img(n) - prediction;            % Difference between actual pixel value and prediction
-            errorQuantized(n) = quantize(predictionError);    % Quantize the prediction error
-            prediction = prediction + errorQuantized(n);      % Update prediction
+                predictionError = img(n) - prediction;            % Difference between actual pixel value and prediction
+                errorQuantized(n) = quantize(predictionError, level);    % Quantize the prediction error
+                prediction = prediction + errorQuantized(n);      % Update prediction
             end
-            
-        else 
+        elseif strcmp(mode,'intuitive') == 1                      % Intuitive way - Transmitting the difference image
             diff = difim(img); 
-            errorQuantized = [diff(1); quantize(diff(2:end))]; 
+            
+            errorQuantized = [diff(1); quantize(diff(2:end), level)]; 
         end
         
-        
-        
-        diff = reshape(errorQuantized, [row, col])';
-        difft = diff';                          % Cumsum traverses column-wise
+        errorQuantized = reshape(errorQuantized, [row, col])';
+        difft = errorQuantized';                          
         predict = cumsum(difft(:));
         img_R = reshape(predict, [col, row])';
 
-
-        
-        
-    case 'rowpixel'
+case 'rowpixel' % Transmitting first pixel of every row
         errorQuantized = [img_O(:,1) zeros(row,col-1)];
         
-        for n=1:row
-            prediction = img_O(n,1); 
-            for i=2:col
-            predictionError = img_O(n,i) - prediction; 
-            errorQuantized(n,i) = quantize(predictionError);
-            prediction = prediction + errorQuantized(n,i);
+        if strcmp(mode,'DPCM') == 1
+            for i=1:row
+                prediction = img_O(i,1); 
+                for j=2:col
+                    predictionError = img_O(i,j) - prediction; 
+                    errorQuantized(i,j) = quantize(predictionError, level);
+                    prediction = prediction + errorQuantized(i,j);
+                end
             end
+            
+            
+        elseif strcmp(mode,'intuitive') == 1                % Intuitive way - Transmitting the difference image
+            errorQuantized = zeros(row,col); 
+            for i=1:row
+                previousPixel = img_O(i,1); 
+                for j=2:col
+                    errorQuantized(i,j) = img_O(i,j) - previousPixel;  
+                    previousPixel = img_O(i,j); 
+                end
+            end
+            errorQuantized = [img_O(:,1) quantize(errorQuantized(:,2:end), level)];
+  
         end
-        
         img_R = cumsum(errorQuantized, 2); 
         
 end
@@ -72,29 +81,24 @@ end
 
 
 
-% 
 
 
+% errorReceiver = (img_O-img_R);
+% meanError = mean(abs(errorReceiver(:)))
 
 
-% % noloop version of reciever
-% 
-
-% 
-% errorReceiver = (img-predict);
-% meanError = mean(abs(errorReceiver))
-% 
-
-imshow(img_R,[])
-subplot(1,3,1)
+subplot(2,2,1)
 imshow(img_O, [])
 title('Original image')
-subplot(1,3,2)
+subplot(2,2,2)
 imshow(img_R,[])
 title('Reconstructed image')
-subplot(1,3,3)
+subplot(2,2,3)
 imshow(img_R-double(img_O), [])
 title('Difference')
+subplot(2,2,4)
+imshow(errorQuantized, [0 1])
+title('Quantized error')
 
 
 
--- Project/quantize.m
+++ Project/quantize.m
@@ -1,20 +1,29 @@
-function q = quantize(d)
-% Quantizer function. 
-  % f(x<=low) = -e1
-  % f(low<x<0) = -e2
-  % f(0<=x<high) = e2
-  % f(high<x) = e1
-  
-  high = 0;
-  low = -high;
-  e1 = 10;
-  e2 = e1;
-  
-  q = -e1*ones(size(d));
-  q(d>low) = -e2;
-  q(d>=0) = e2;
-  q(d>=high) = e1;
-  
+function q = quantize(d, levels)
+q = zeros(size(d)); 
+
+switch levels
+    case '1bit'
+        input = 0;
+        output = 10; 
+        q(d <= input) = -output; 
+        q(d > input) = output; 
+    case '2bit'
+        
+    case '3bit'
+        
+    case '4bit'
+
+% L1 = 1; 
+% L2 = 5; 
+% 
+% q(d < -2) = -L2; 
+% q(d >= -2 & d < 0) = -L1; 
+% 
+% q(d >= 0 & d < 2) = L1; 
+% q(d >= 2) = L2; 
+
+
+
 
 end
 
